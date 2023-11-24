f57ee1105d1c07cc1535be946b30c123029113fd
Kalisvar <kalisvar1408@gmail.com>
2023-11-24 12:36:06 +0530

    basics question paper codes added and hello world deleted

--- dummy/Program.cs
+++ dummy/Program.cs
@@ -1,16 +1,195 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace dummy
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            //Question no (1)
+            //Console.WriteLine("Enter the first Integer:");
+            //int firstinteger = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Second Integer:");
+            //int secondinteger = int.Parse(Console.ReadLine());
+            //int Sum = firstinteger + secondinteger;
+            //int difference = Math.Abs(firstinteger - secondinteger);
+            //int  product = firstinteger * secondinteger;
+            //Console.WriteLine("Sum of two Integer:" + Sum + "\nDifference Of two integer:" + difference + "\nProduct of Two integer: " + product);
+
+            // Question no(2)
+            //Console.WriteLine("Enter the value a(positive Integer):");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the value b(positive Integer):");
+            //int b = int.Parse(Console.ReadLine());
+            //double CalculatedValue = Math.Pow(a,b);
+            //Console.WriteLine("The value of " + a + " in times of " + b + " is " + CalculatedValue);  
+
+            //Question no (3)
+            //Console.WriteLine("Enter the Length:");
+            //int length = int.Parse(Console.ReadLine()); 
+            //Console.WriteLine("Enter the Width:");
+            //int width = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Height:");
+            //int height =int.Parse(Console.ReadLine());
+            //long Volume = length * width * height;
+            //Console.WriteLine("The volume of the tank is :" + Volume);
+
+            //Question no (4)
+            //Console.WriteLine("Enter the Radius:");
+            //float radius = float.Parse(Console.ReadLine());
+            //double Perimeter = 2 * Math.PI * radius;
+            //Console.WriteLine("The perimeter of a circle is :" + string.Format("{0:0.00}", Perimeter));
+
+            //Question no (5)
+            //Console.WriteLine("Enter the numerator:");
+            //int Numerator = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Denominator:");
+            //int Denominator = int.Parse(Console.ReadLine());
+            //int Quotient = Numerator/Denominator;
+            //int Remainder = Numerator%Denominator;
+            //Console.WriteLine("quotient is :" +Quotient);
+            //Console.WriteLine("Remainder is :" +Remainder);
+
+            //Question(6)
+            //Console.WriteLine("Enter the Number:");
+            //int number = int.Parse(Console.ReadLine());
+            //double calculatedvalue = Math.Pow(number, 2);
+            //Console.WriteLine("The square of number is: " + calculatedvalue); 
+
+
+            //Question (7):
+            //Console.WriteLine("Enter the Number:");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("The square root of" + number + " is :" + Math.Sqrt(number));
+
+            //Question (8):
+            //Console.WriteLine("Enter the Bredth:");
+            //int bredth = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the height:");
+            //int height = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Area of triangle is " + ((1.0 / 2) * (bredth * height)));
+
+            //Question (9):
+            //Console.WriteLine("Enter the kilometer:");
+            //float number = int.Parse(Console.ReadLine());
+            //double convertedvalue = ((number) * (0.621371));
+            //Console.WriteLine("mile of " + number +  " kilomter:" +convertedvalue);
+
+            //Question (10):
+            //Console.WriteLine("Enter the celsius:");
+            //float number = int.Parse(Console.ReadLine());
+            //double convertedvalue = ((number * (1.8)) + 32);
+            //Console.WriteLine("celsius of " + number + " fahrenheit:" + convertedvalue);
+
+
+            //Question (11)
+            //Console.WriteLine("Enter the First variable: ");
+            //int variable1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Second variable: ");
+            //int variable2 = int.Parse(Console.ReadLine());
+            //int variable3 = variable2;
+            //variable2 = variable1;
+            //variable1 = variable3;
+            //Console.WriteLine("swapped variable:" + variable1 + "and" + variable2);
+
+
+            //Question (12)
+            //Console.WriteLine("Enter the First variable: ");
+            //int variable1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Second variable: ");
+            //int variable2 = int.Parse(Console.ReadLine());
+            // variable1 = variable1 + variable2;
+            // variable2 = variable1 - variable2;
+            // variable1 = variable1 - variable2;
+            // Console.WriteLine("swapped variable:" + variable1+ "and" + variable2);
+
+
+            //Question (13)
+            //Console.WriteLine("Enter the First variable: ");
+            //int variable1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Second variable: ");
+            //int variable2 = int.Parse(Console.ReadLine());
+            //variable1 = variable1^variable2;
+            //variable2 = variable1^variable2;
+            //variable1 = variable1^variable2;
+            //Console.WriteLine("swapped variable:" + variable1 + "and" + variable2);
+
+            //Question(14)
+            //Random random = new Random();
+            //int result = random.Next(1,100);
+            //Console.WriteLine("Random Number  between 1 and 100 is " +result);
+
+
+            //Question(15):
+            //Console.WriteLine("Enter the length in feet:");
+            //int length = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the width in feet:");
+            //int Width = int.Parse(Console.ReadLine());
+            //double number = length * Width;
+            //double value =  number/ 43560;
+            //Console.WriteLine("Area of field in acres is :" +value);
+
+            //Question(16):
+            //Console.WriteLine("Enter the Number N :");
+            //int number = int.Parse(Console.ReadLine());
+            //int sum = (number * (number + 1) / 2);
+            //Console.WriteLine( "Sum of Number N is :" +sum);
+
+
+            //Question (17):
+            //Console.WriteLine("Enter the number1:");
+            //int number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number2:");
+            //int number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number3:");
+            //int number3 = int.Parse(Console.ReadLine());
+            //int Value = (number1 + number2 + number3) / 3;
+            //Console.WriteLine("Average is :" + Value);
+
+
+
+            //Question (18)
+            //Console.WriteLine("Enter the Bill amount: ");
+            //int amount = int.Parse(Console.ReadLine());
+            //double Tipamount = amount * 0.05;
+            //Console.WriteLine("Tip for the waiter is " + Tipamount);
+
+
+            //Question(19)
+            //Console.WriteLine("Enter the total Bill amount: ");
+            //int totalamount = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the tax percentage: ");
+            //int tax = int.Parse(Console.ReadLine());
+            //double taxamount = totalamount*(tax * 0.01);
+            //double netamount = totalamount - taxamount;
+            //Console.WriteLine("Tip for the waiter is " +netamount);
+
+            //Question (20)
+            //Console.WriteLine("Enter the basic Pay");
+            //int basicpay = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Dearness Allowance Percent");
+            //int DA = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Travelling  Allowance Percent");
+            //int TA = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Provident Fund Percnet");
+            //int PF = int.Parse(Console.ReadLine());
+            //double Grosspay = basicpay + (basicpay * DA * 1.0 / 100) + (basicpay * TA * 1.0 / 100) - (basicpay * PF * 1.0 / 100);
+            //Console.WriteLine("Grosspay: " + Grosspay);
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
